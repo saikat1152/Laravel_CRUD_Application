b5f08365ad1f3660d6481d1c418e0682ea903a9a
saikatsafiq <saikat25.g@gmail.com>
2019-05-21 01:52:29 +0600

    Student Crud Application in laravel

---
+++ resources/views/create.blade.php
@@ -0,0 +1,95 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">
+
+        @if($errors->any())
+             @foreach($errors->all() as $error)
+                <div class="alert alert-dismissible alert-danger">
+                    <button type="button" class="close" data-dismiss="alert">×</button>
+                    <strong>Sorry!</strong>{{ $error }}
+                </div>
+             @endforeach
+         @endif
+
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">Add New Student</h3>
+            </div>
+
+            <div class="panel-body">
+                <form class="form-horizontal" action="{{route('store')}}" method="POST">
+                    {{ csrf_field() }}
+                    <fieldset>
+                        {{--<legend>Legend</legend>--}}
+                        <div class="form-group">
+                            <label for="firstname" class="col-md-2 control-label">First Name</label>
+
+                            <div class="col-md-10">
+                                <input type="text" class="form-control" name="firstname" placeholder="First Name">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="lastname" class="col-md-2 control-label">Last Name</label>
+
+                            <div class="col-md-10">
+                                <input type="text" class="form-control" name="lastname" placeholder="Last Name">
+                            </div>
+                        </div>
+                        {{--<div class="form-group">--}}
+                            {{--<label for="inputPassword" class="col-md-2 control-label">Password</label>--}}
+
+                            {{--<div class="col-md-10">--}}
+                                {{--<input type="password" class="form-control" id="inputPassword" placeholder="Password">--}}
+
+                            {{--</div>--}}
+                        {{--</div>--}}
+                        <div class="form-group">
+                            <label for="inputEmail" class="col-md-2 control-label">Email</label>
+
+                            <div class="col-md-10">
+                                <input type="email" class="form-control" name="email" id="inputEmail" placeholder="Email">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="phone" class="col-md-2 control-label">Mobile Number</label>
+
+                            <div class="col-md-10">
+                                <input type="text" class="form-control" name="phone" placeholder="Phone">
+                            </div>
+                        </div>
+
+                        {{--<div class="form-group">--}}
+                            {{--<label for="inputFile" class="col-md-2 control-label">File</label>--}}
+
+                            {{--<div class="col-md-10">--}}
+                                {{--<input type="text" readonly="" class="form-control" placeholder="Browse...">--}}
+                                {{--<input type="file" id="inputFile" multiple="">--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                        <div class="form-group">
+                            <label for="address" class="col-md-2 control-label">Address</label>
+
+                            <div class="col-md-10">
+                                <textarea class="form-control" rows="2" name="address" id="textArea"></textarea>
+                                <span class="help-block">A longer block of help text that breaks onto a new line and may extend beyond one line.</span>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-10 col-md-offset-2">
+                                <button type="button" class="btn btn-default">Cancel</button>
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
+
+            </div>
+
+        </div>
+
+    </div>
+
+@endsection
